57303009598e46145fffe5f084a64f30cad6430f
William Ku <kucheech@gmail.com>
2016-07-01 22:11:00 +0800

    included the OLD API key

--- app/src/main/java/sg/com/kaplan/pdma/neaexample/MainActivity.java
+++ app/src/main/java/sg/com/kaplan/pdma/neaexample/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends ActionBarActivity {
         //please download your API keys from https://www.nea.gov.sg/api
         String dataset = "nowcast";
         //String dataset = "12hrs_forecast";
-        String keyref = "";
+        String keyref = "781CF461BB6606ADEA6B1B4F3228DE9D586412BA7716CB8F";
 
         String url = "http://www.nea.gov.sg/api/WebAPI?dataset=" + dataset + "&keyref=" + keyref;
         AsyncHttpClient client = new AsyncHttpClient();
